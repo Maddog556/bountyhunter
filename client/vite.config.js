941e7b42a1b3ff80ed49d43b48f680e883cc454e
josh maddox <maddoxjosh310@yahoo.com>
2023-08-08 16:49:49 -0400

    delete

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,14 +6,14 @@ import react from '@vitejs/plugin-react'
  //https://vitejs.dev/config/server-options.html#server-proxy for example for mulitple Router
 
  export default defineConfig({
-  server: {
-    proxy: {
-      // put target start params example localhost:9000/test 
-      '/bountiesList': {
-        target: 'http://localhost:9000/',
-        changeOrigin: true,
-      },
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     // put target start params example localhost:9000/test 
+  //     '/bountiesList': {
+  //       target: 'http://localhost:9000/',
+  //       changeOrigin: true,
+  //     },
+  //   }
+  // },
   plugins: [react()],
 })
