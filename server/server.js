175deb02c38a37ccc75e84acecac1715ea9e2667
josh maddox <maddoxjosh310@yahoo.com>
2023-08-08 16:23:00 -0400

    deploy 1

--- server/server.js
+++ server/server.js
@@ -3,20 +3,29 @@ const morgan = require("morgan")
 const mongoose = require("mongoose")
 const app = express()
 require("dotenv").config()
+uri = process.env.URI
+process.env.SECRET
 
 // Middleware (for every request)
 app.use(express.json()) //looks for a request body, ant turns it into 'req.body'
 app.use(morgan('dev'))
 
+app.use(express.static(path.join(__dirname, "client", "dist")))
+
+
 // connect to data base 27017 is the default port for local host in mongodb 
 mongoose.set('strictQuery', false)
-mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.3mv0url.mongodb.net/bountydb`,
+mongoose.connect(uri,
 () => console.log("connected to the data base")
 )
 
 
 // Routes
 app.use("/bountiesList", require("./routes/bountyRouter.js"))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 //Error handler and to have these 4 params 
 app.use((err,req,res,next) => {
