72297137ede3d0b926f9a0d77d2ad0ce1ad60557
josh maddox <maddoxjosh310@yahoo.com>
2023-08-08 11:37:42 -0400

    first one

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from "react"
+import Bounty from "./components/Bounty.jsx"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Bounty/>
+    </>
+  )
+}
+
+export default App
